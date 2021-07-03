e283c3f42bb958b3041bf4a073868c6a1964b5c0
Papoui <darckrim64@gmail.com>
2021-07-03 14:54:27 +0200

    Modification du main pour séparer les commandes du fichier principal - Ajout de la commande pute.js

---
+++ commands/Interactions/pute.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'pute',
+	description: 'salope saloepolf',
+	execute(msg) {
+		msg.channel.send('Salope salope');
+	},
+};
--- main.js
+++ main.js
@@ -1,15 +1,46 @@
+const fs = require('fs');
 const Discord = require('discord.js');
-const client = new Discord.Client();
-const config = require('./config.json')
+const { PREFIX, TOKEN } = require('./config.json');
 
-client.on('ready', () => {
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFolder = fs.readdirSync('./command')
+for (folder of commandFolder){
+	
+	const commandFiles = fs.readdirSync(`./command/${folder}`).filter(file => file.endsWith('.js'));
+
+	for (const file of commandFiles) {
+		const command = require(`./commands/${folder}/${file}`);
+		client.commands.set(command.name, command);
+	}
+}
+
+
+
+
+client.once('ready', () => {
 	console.log('Ready!');
 });
 
 client.on('message', msg => {
-	if(msg.content === '**pute pute'){
-		msg.channel.send('salope salope')
+	if (!msg.content.startsWith(PREFIX) || msg.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const commandName = args.shift().toLowerCase();
+
+	if (!client.commands.has(commandName)) return;
+
+	const command = client.commands.get(commandName);
+
+	try {
+		command.execute(message, args);
+	} catch (error) {
+		// ...
 	}
+
 });
 
-client.login(config.TOKEN);
+client.login(TOKEN);
+const r34 = require('api-rule34-xxx')
+console.log(r34.getPost(4868261))
